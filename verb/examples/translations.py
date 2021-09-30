9eb747b533b42ed6cd9de482e4095337b24a9622
Thor Whalen <thorwhalen1@gmail.com>
2021-09-29 21:27:53 -0700

    feat: a new example module: translations.py

---
+++ verb/examples/translations.py
@@ -0,0 +1,57 @@
+"""This module demos how to interpret a different minilanguage by translating it
+to the verb framework"""
+
+from verb import Command, Literal
+from inspect import signature
+
+
+def mk_minilanger():
+    def command_dict(input_val, spec: dict):
+        operator, tag, value = spec['operator'], spec['tag'], spec['value']
+        if not isinstance(input_val, Literal):
+            input_val = Literal(input_val)
+        return {operator: ({'[]': (input_val, tag)}, value)}
+
+    def command(command_dict, interpreter_dict):
+        return Command.from_dict(command_dict, interpreter_dict)
+
+    def result(command):
+        return command()
+
+    from meshed import DAG
+
+    minilanger = DAG([result, command_dict, command])
+    return minilanger
+
+
+def test_minilanger():
+    minilanger = mk_minilanger()
+    assert str(signature(minilanger)) == '(input_val, spec: dict, interpreter_dict)'
+    minilanger.dot_digraph()
+
+    import operator as o
+    from functools import partial
+
+    func_of_key = {
+        '>': o.gt,
+        '<': o.lt,
+        '>=': o.ge,
+        '<=': o.le,
+        '==': o.eq,
+        '[]': lambda d, tag: d[tag]
+        # 'call': lambda f, x: f(x)
+    }
+
+    minilang = partial(minilanger, interpreter_dict=func_of_key)
+
+    func1 = partial(minilang, spec={'tag': 'temperature', 'operator': '<', 'value': 0})
+    assert func1({'temperature': 2}) == False
+    assert func1({'temperature': -3}) == True
+    assert func1({'temperature': -10, 'rpm': 234}) == True
+
+    func2 = partial(
+        minilang, spec={'tag': 'machine', 'operator': '==', 'value': 'fridge'}
+    )
+    assert func2({'machine': 'fridge'}) == True
+    assert func2({'machine': 'coffee maker'}) == False
+    assert func2({'temperature': -10, 'machine': 'fridge', 'color': 'blue'}) == True
